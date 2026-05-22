8a290a037600b618a03e48a7e5b9d7bac134a45e
sShugen <hotsuper75@gmail.com>
2026-05-22 10:52:00 +0700

    Delete bai3.py

--- bai3.py
+++
@@ -1,8 +0,0 @@
-number_of_classroom = int(input("Số lượng phòng họp cần kiểm tra: "))
-number_of_chair = int(input("Số lượng ghế của từng phòng: "))
-number_of_chair_each_row = int(input("Số ghế mỗi hàng: "))
-
-for i in range(1 , number_of_chair_each_row + 1):
-    for j in range(1 , number_of_chair + 1):
-        print("*" , end="")
-    print()
